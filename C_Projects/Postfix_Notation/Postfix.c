1ea5ea45fdfe52d54490f249813ed52271c27610
Gabe <gadimas@ucsc.edu>
2022-02-03 18:56:01 -0800

    added postfix notation

--- C_Projects/Postfix_Notation/Postfix.c
+++ C_Projects/Postfix_Notation/Postfix.c
@@ -9,87 +9,32 @@ Expression *make_new_expression(void){
 	return ptr;
 }
 
-bool add_number(Expression *exp, int num){
-	//if(!check_expression(exp)){return false;}
+bool add_char(Expression *exp, char car){
+	int outcome = car;
+	if(!check_character(car)){//if an operator(returns false), then perform an operation
+		print_expression(exp);
+		printf("%d %c %d -> ", exp->top->prev->data, car, exp->top->data);
+		outcome = do_operation(exp->top->prev->data, exp->top->data, car);
+		printf("%d You have %d operations left!\n", outcome, exp->size-1);
+		
+		exp->size--;
+	} else{
+		exp->size++;
+	}
 	Character *newtop = (Character *)calloc(1, sizeof(Character));
-	newtop->intVal = num;			//assigns the number
+	newtop->data = outcome;			//assigns the number
 	newtop->prev = exp->top;	//newtop.prev gets existing top
 	if(exp->top != NULL){
 		exp->top->next = newtop;	//existing top.next gets new top
 	}
-	exp->top = newtop;			//assigns the new top
-	exp->size++;
-	exp->nums++;
-	return true;		
-}
-
-bool add_operator(Expression *exp, char op){
-	//if(!check_expression(exp)){return false;}
-	Character *newtop = (Character *)calloc(1, sizeof(Character));
-	newtop->opVal = op;			//assigns the number
-	newtop->prev = exp->top;	//newtop.prev gets existing top
-	if(exp->top != NULL){
-		exp->top->next = newtop;	//existing top.next gets new top
-	}
-	exp->top = newtop;			//assigns the new top
-	exp->size++;
-	exp->ops++;
-	return true;		
-}
-
-bool delete_values(Expression *exp, Character *value, int newValue, Expression *initAdrs){
-	if(!check_expression(exp)){return false;}
-	Expression *newExp = make_new_expression(); //creates the new expresssion with size of original expression - 2
-	int stop = exp->size, ctr = 0;		//gets the size of the list
-	Character *elt = exp->top;				//starts off at the top of the list
-	while(ctr != stop-1){
-		elt = elt->prev; 				//gets the first elt of the list has to circle back
-		ctr++;
-	}	//elt is now the address at the bottom of the stack
-	//we now know that the number of integers reduced by 2 and the number of operators reduced by 1
-	for(int i = 0; i < exp->nums-2; i++){	
-		add_number(newExp, elt->intVal);	
-		elt = elt->next;
-	}	//adds the current numbers to the list (not evaluated)
-	add_number(newExp, newValue);	//adds the new value
-	elt = exp->top;					//resets back to the top of the stack
-	for(int i = 0; i < exp->ops-1; i++){
-		add_operator(newExp, elt->opVal);
-		elt = elt->prev;
-	}
-	
-	//make a new expression with the values of the old one and size - 2
-	free(exp);//free the memory and assign exp to the new expression
-	printf("Adrs of initAdrs: %p, Adrs of newExp: %p\n", initAdrs, newExp);
-	*initAdrs = *newExp;
-	//*initAdrs = *(Expression *)newExp;
-	//*exp = *newExp;	//exp gets the new address of the allocated memory
-	printf("Adrs of initAdrs: %p, Adrs of newExp: %p\n", initAdrs, newExp);
-	return true;
-
+	exp->top = newtop;			//assigns the new top	
+	return true;	
 }
 
 
 
-float get_reduced(Expression *exp, Expression **initAdrs){	//initAdrs is the original address of the expression
-	if(check_expression(exp)){
-		float val;
-		Character *value = exp->top;	//takes the first operation added to the list
-		for(int i = 0; i < exp->ops-1; i++){
-			value = value->prev;
-		}	//now value has the very first operation
-		Character *temp = value->prev;
-		int num2 = temp->intVal;	//gets the second number
-		int num1 = temp->prev->intVal;//gets the first number
-		val = do_operation(num1, num2, value->opVal);
-		delete_values(exp, temp, val, (Expression *)initAdrs);	//is the middle of num1, num2(temp), operator
-		return val;
-	}
-	return 0;
-}
 
-
-float do_operation(int num1, int num2, char op){
+int do_operation(int num1, int num2, char op){
 	switch(op){
 		case '+':
 			return num1 + num2;
@@ -110,59 +55,48 @@ float do_operation(int num1, int num2, char op){
 }
 
 bool print_expression(Expression *exp){
-	int stop = exp->size, ctr = 0;		//gets the size of the list
-	Character *elt = exp->top;				//starts off at the top of the list
-	/*while(ctr != stop-1){
-		elt = elt->prev; 				//gets the first elt of the list has to circle back
+	int stop = exp->size, ctr = 0;
+	Character *ptr = exp->top;
+	while(ctr != stop-1){
+		ptr = ptr->prev;
 		ctr++;
-	}*/
-	//printf("[");
-	for(int i = 0; i < stop; i++)
-	{
-		if(elt->intVal != 0){	//is an integer
-			printf("%d, ", elt->intVal);
-		} else {
-			printf("%c, ", elt->opVal);
-		}
-		printf("Adrs of Current: %p, Adrs of previous: %p, Adrs of Next: %p\n", elt, elt->prev, elt->next);
-		elt = elt->prev;						//goes backwards in the list (from top to bottom of the List)
 	}
-	//printf("]\n");
+	printf("[");
+	for(int i = 0; i < exp->size; i++){
+		printf("%d ", ptr->data);
+		ptr = ptr->next;
+	}
+	printf("] ");
 	return true;
 }
 
-
-bool check_expression(Expression *exp){
-	if(exp->top == NULL){	
-		printf("There is something wrong with the top of the stack!\n");
-		return false;
+bool check_character(char op){
+	switch(op){
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+			return false;
+			break;		
+		default:
+			return true;
 	}
-	if(exp->nums - 1 != exp->ops){
-		printf("Error: mismatch operations and operands!\n");
-		printf("%d Numbers, %d Operations\n", exp->nums, exp->ops);
-		return false;
-	}
-	return true;
 }
 
 
 
 
 int main(void){
-	Expression *myExpression = make_new_expression();
-	printf("Original Pointer: %p, Original address: %p\n", myExpression, &myExpression);
-	add_number(myExpression, 12);
-	add_number(myExpression, 10);
-	add_number(myExpression, 2);
-	add_operator(myExpression, '/');
-	add_operator(myExpression, '-');
-	print_expression(myExpression);
-	printf("First reduction: %.3f\n", get_reduced(myExpression, &myExpression));
-	printf("New address: %p\n", myExpression);
-	print_expression(myExpression);
-	printf("New Pointer: %p, Original address: %p\n", myExpression, &myExpression);
-	printf("Second reduction: %.3f\n", get_reduced(myExpression, &myExpression));
-	//print_expression(myExpression);
+Expression *myExpression = make_new_expression();
+	while(true){
+		int input;
+		printf("Enter a value: ");
+		scanf("%s", &input);
+		add_char(myExpression, input);
+		print_expression(myExpression);
+		printf("\n");
+	}
+
 	
 
 	return 0;
--- C_Projects/Postfix_Notation/Postfix.h
+++ C_Projects/Postfix_Notation/Postfix.h
@@ -4,8 +4,7 @@
 typedef struct Expression Expression;
 typedef struct Character Character;
 struct Character{
-	int intVal;
-	char opVal;						//can be a char or int
+	int data;
 	Character *next;
 	Character *prev;
 };
@@ -13,25 +12,17 @@ struct Character{
 struct Expression{
 	Character *top;
 	int size;
-	int nums;
-	int ops;
 };
 
 Expression *make_new_expression(void);
 
-bool add_number(Expression *exp, int num); //adds a number to the expression
+bool add_char(Expression *exp, char num); //adds a number to the expression
 													//return true if added successfully, else false
 
-bool add_operator(Expression *exp, char op);
-
-float get_reduced(Expression *exp, Expression **initAdrs);	//returns the reduced form of operation only of first term
-
-bool check_expression(Expression *exp);	//checks if we can perform an operation
+bool check_character(char op);	//checks if we can perform an operation
 												//also checks if opperators are in the correct order									
-void clear_expression(Expression *exp);	//clears the expression to make a new one
 
 bool print_expression(Expression *exp);	//prints the entire expression
 
-bool delete_values(Expression *exp, Character *value, int newValue, Expression *initAdrs);		//pops an operator to the list
 
-float do_operation(int n1, int n2, char op);		//does the operation
+int do_operation(int n1, int n2, char op);		//does the operation
